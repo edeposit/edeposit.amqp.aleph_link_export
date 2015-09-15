5f382461f189dc966235240692715a4620cfb825
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-15 17:05:32 +0200

    Added sketch of the RequestDatabase.

--- src/edeposit/amqp/aleph_link_export/link_export.py
+++ src/edeposit/amqp/aleph_link_export/link_export.py
@@ -4,7 +4,11 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from settings import REQUEST_DIR
+from settings import RESPONSE_DIR
+from settings import DATABASE_DIR
 
+from xmltodict import unparse
 
 
 # Variables ===================================================================
@@ -12,6 +16,25 @@
 
 
 # Functions & classes =========================================================
+class RequestDatabase(object):
+    def __init__(self, req_path=REQUEST_DIR, resp_path=RESPONSE_DIR):
+        self.req_path = req_path
+        self.resp_path = resp_path
+
+        self.req_queue = []
+        self.resp_queue = []
+        self.log = []
+
+    def add_request(self, request):
+        self.req_queue.append(request)
+
+    def to_xml(self):
+        return unparse(
+            {"records": self.req_queue},
+            pretty=True
+        )
+
+
 def export(request):
     pass
 
