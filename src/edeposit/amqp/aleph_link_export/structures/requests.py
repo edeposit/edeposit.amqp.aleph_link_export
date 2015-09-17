d346dc9ed8461f0364041f2031d4a09874503c48
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-17 13:41:45 +0200

    #3: Added parameter ._session_id into request structure.

--- src/edeposit/amqp/aleph_link_export/structures/requests.py
+++ src/edeposit/amqp/aleph_link_export/structures/requests.py
@@ -13,19 +13,23 @@ from odictliteral import odict
 class LinkUpdateRequest(namedtuple("LinkUpdateRequest", ["uuid",
                                                          "doc_number",
                                                          "document_url",
-                                                         "kramerius_url"])):
-    def __new__(cls, uuid, doc_number, document_url, kramerius_url=None):
+                                                         "kramerius_url",
+                                                         "_session_id"])):
+    def __new__(cls, uuid, doc_number, document_url, _session_id,
+                kramerius_url=None):
         return super(LinkUpdateRequest, cls).__new__(
             cls,
-            uuid,
-            doc_number,
-            document_url,
-            kramerius_url
+            uuid=uuid,
+            doc_number=doc_number,
+            document_url=document_url,
+            kramerius_url=kramerius_url,
+            _session_id=_session_id
         )
 
     def to_dict_xml(self):
         record = odict[
             "record": odict[
+                "@session_id": self._session_id,
                 "uuid": self.uuid,
                 "doc_number": self.doc_number,
                 "kramerius_url": self.kramerius_url,
