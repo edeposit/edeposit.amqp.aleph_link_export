8ef7cd17df2e405112ab2f17510e8d6e83f49b11
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-17 17:11:31 +0200

    #4: Added test templates.

---
+++ tests/test_RequestDatabase.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import pytest
+
+
+# Variables ===================================================================
+
+
+
+# Fixtures ====================================================================
+# @pytest.fixture
+# def fixture():
+#     pass
+
+# with pytest.raises(Exception):
+#     raise Exception()
+
+
+# Tests =======================================================================
+def test_():
+    pass
