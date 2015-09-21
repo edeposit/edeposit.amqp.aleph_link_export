634d11710cebe77e759ed3e5f4f0b079b76c9cd0
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-21 15:59:44 +0200

    Added setup.py template.

---
+++ setup.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from setuptools import setup, find_packages
+from docs import getVersion
+
+
+# Variables ===================================================================
+changelog = open('CHANGELOG.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    changelog
+])
+
+
+# Actual setup definition =====================================================
+setup(
+    name='edeposit.amqp.aleph_link_export',
+    version=getVersion(changelog),
+    description="Subsystem for updating Edeposit's links in Aleph.",
+    long_description=long_description,
+    url='https://github.com/edeposit/edeposit.amqp.aleph_link_export/',
+
+    author='Edeposit team',
+    author_email='edeposit@email.cz',
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+
+        "License :: OSI Approved :: MIT License",
+    ],
+    license='MIT',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['edeposit', 'edeposit.amqp'],
+
+    # scripts=[
+    #     'bin/edeposit_storage_runzeo.sh',
+    #     'bin/edeposit_storage_server.py',
+    # ],
+
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+
+    test_suite='py.test',
+    tests_require=["pytest"],
+    extras_require={
+        "test": [
+            "pytest",
+            "fake-factory"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
+)
