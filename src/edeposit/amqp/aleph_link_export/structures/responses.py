8434be1aacca56aee44bdf8f8adea036faa90aca
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-22 17:08:41 +0200

    #6: Added docstring for LinkUpdateResponse.

--- src/edeposit/amqp/aleph_link_export/structures/responses.py
+++ src/edeposit/amqp/aleph_link_export/structures/responses.py
@@ -10,4 +10,14 @@ from collections import namedtuple
 # Requests ====================================================================
 class LinkUpdateResponse(namedtuple("LinkUpdateResponse", ["status",
                                                            "session_id"])):
-    pass
+    """
+    Response to the :class:`.LinkUpdateRequest` request.
+
+    This object is returned only when the Aleph signals, that the record was
+    really updated.
+
+    Attributes:
+        status (str): Status of the update request. Either ``OK``, or
+                      ``ERROR``.
+        session_id (str): Corresponding session id.
+    """
