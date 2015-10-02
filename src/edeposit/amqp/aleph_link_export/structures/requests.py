23c0f7f9b4224cb6cac1b99e080d8ef6dacaaba7
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-02 13:26:43 +0200

    #12: document_url property changed to list.

--- src/edeposit/amqp/aleph_link_export/structures/requests.py
+++ src/edeposit/amqp/aleph_link_export/structures/requests.py
@@ -12,7 +12,7 @@ from odictliteral import odict
 # Requests ====================================================================
 class LinkUpdateRequest(namedtuple("LinkUpdateRequest", ["uuid",
                                                          "doc_number",
-                                                         "document_url",
+                                                         "document_urls",
                                                          "kramerius_url",
                                                          "urn_nbn",
                                                          "session_id"])):
@@ -24,20 +24,23 @@ class LinkUpdateRequest(namedtuple("LinkUpdateRequest", ["uuid",
                    with :mod:`.responses`.
         uuid (str): UUID for the `doc_number` you wish to update.
         doc_number (str): Document number of the document you wish to update.
-        document_url (str): Newly added public URL to the storage / whatever
+        document_urls (list): Newly added public URL to the storage / whatever
                      subsystem.
         kramerius_url (str, default None): Newly added URL to the Kramerius
                       subsystem.
         urn_nbn (str, default None): Optional newly added URN:NBN for the
                                      record.
     """
-    def __new__(cls, uuid, doc_number, document_url, session_id, urn_nbn=None,
+    def __new__(cls, uuid, doc_number, document_urls, session_id, urn_nbn=None,
                 kramerius_url=None):
+        if type(document_urls) not in [list, tuple]:
+            document_urls = [document_urls]
+
         return super(LinkUpdateRequest, cls).__new__(
             cls,
             uuid=uuid,
             doc_number=doc_number,
-            document_url=document_url,
+            document_urls=document_urls,
             kramerius_url=kramerius_url,
             urn_nbn=urn_nbn,
             session_id=session_id
@@ -57,7 +60,7 @@ class LinkUpdateRequest(namedtuple("LinkUpdateRequest", ["uuid",
             "doc_number": self.doc_number,
             "urn_nbn": self.urn_nbn,
             "kramerius_url": self.kramerius_url,
-            "document_url": self.document_url,
+            "document_urls": self.document_urls,
         ]
 
         if not self.kramerius_url:
--- tests/structures/test_requests.py
+++ tests/structures/test_requests.py
@@ -17,7 +17,7 @@ def link_update_req():
         uuid="uuid",
         doc_number="doc_number",
         urn_nbn="urn_nbn",
-        document_url="document_url",
+        document_urls="document_urls",
         kramerius_url="kramerius_url",
         session_id="session_id",
     )
@@ -33,7 +33,7 @@ def test_LinkUpdateRequest_init(link_update_req):
     assert link_update_req.uuid == "uuid"
     assert link_update_req.doc_number == "doc_number"
     assert link_update_req.urn_nbn == "urn_nbn"
-    assert link_update_req.document_url == "document_url"
+    assert link_update_req.document_urls == ["document_urls"]
     assert link_update_req.kramerius_url == "kramerius_url"
     assert link_update_req.session_id == "session_id"
 
@@ -42,13 +42,13 @@ def test_LinkUpdateRequest_init_without_kramerius():
     lur = LinkUpdateRequest(
         uuid="uuid",
         doc_number="doc_number",
-        document_url="document_url",
+        document_urls="document_urls",
         session_id="session_id",
     )
 
     assert lur.uuid == "uuid"
     assert lur.doc_number == "doc_number"
-    assert lur.document_url == "document_url"
+    assert lur.document_urls == ["document_urls"]
     assert lur.session_id == "session_id"
     assert lur.kramerius_url is None
     assert lur.urn_nbn is None
@@ -62,14 +62,14 @@ def test_LinkUpdateRequest_to_dict_xml(link_update_req):
     assert xml_dict["doc_number"] == link_update_req.doc_number
     assert xml_dict["urn_nbn"] == link_update_req.urn_nbn
     assert xml_dict["kramerius_url"] == link_update_req.kramerius_url
-    assert xml_dict["document_url"] == link_update_req.document_url
+    assert xml_dict["document_urls"] == link_update_req.document_urls
 
 
 def test_LinkUpdateRequest_to_dict_xml_without_kramerius_and_urn(link_update_req):
     lur = LinkUpdateRequest(
         uuid="uuid",
         doc_number="doc_number",
-        document_url="document_url",
+        document_urls="document_urls",
         session_id="session_id",
     )
     xml_dict = lur.to_dict_xml()
@@ -79,7 +79,7 @@ def test_LinkUpdateRequest_to_dict_xml_without_kramerius_and_urn(link_update_req
     assert xml_dict["doc_number"] == link_update_req.doc_number
     assert "kramerius_url" not in xml_dict
     assert "urn_nbn" not in xml_dict
-    assert xml_dict["document_url"] == link_update_req.document_url
+    assert xml_dict["document_urls"] == link_update_req.document_urls
 
 
 def test_status_request(status_request):
